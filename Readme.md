3f189c6ab38e06d4d99af6b6306c7e75169b1ca5
Ralph-Gordon Paul <gordon@rgpaul.com>
2018-11-03 21:57:00 +0100

    Add conan receipe
    Testet under windows

---
+++ Readme.md
@@ -0,0 +1,58 @@
+# Conan zlib script
+
+This repository contains the conan receipe that is used to build the zlib packages at [rgpaul bintray](https://bintray.com/manromen/rgpaul).
+
+For Infos about the zlib library please visit [zlib.net](https://zlib.net).
+The library is licensed under the [zlib License](https://zlib.net/zlib_license.html).
+This repository is licensed under the [MIT License](LICENSE).
+
+## Android
+
+Coming soon
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* [Android NDK r18b](https://developer.android.com/ndk/downloads/)
+
+## Debian 9 (Stretch)
+
+Coming soon
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* build-essential, make, curl, git, unzip and zip (`apt-get install build-essential cmake curl git unzip zip`)
+
+## iOS
+
+Coming soon
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* [Xcode](https://developer.apple.com/xcode/)
+
+## macOS
+
+Coming soon
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* [Xcode](https://developer.apple.com/xcode/)
+
+## Windows 10
+
+To create a package for Windows 10 you can run the conan command like this:
+`conan create . zlib/1.2.11@rgpaul/stable -s os=Windows -s compiler="Visual Studio" -s compiler.runtime=MT -s arch=x86 -s build_type=Release -o shared=False`
+
+### Requirements
+
+* [CMake](https://cmake.org/)
+* [Conan](https://conan.io/)
+* [Visual Studio 2017](https://visualstudio.microsoft.com/de/downloads/)
---
+++ conanfile.py
@@ -0,0 +1,60 @@
+from conans import ConanFile, CMake, tools
+import os
+
+class ZlibConan(ConanFile):
+    name = "zlib"
+    version = "1.2.11"
+    author = "Ralph-Gordon Paul (gordon@rgpaul.com)"
+    settings = "os", "compiler", "build_type", "arch"
+    options = {"shared": [True, False],
+                "android_ndk": "ANY"}
+    default_options = "shared=False", "android_ndk=None"
+    description = "zlib is a lossless data-compression library."
+    url = "https://github.com/Manromen/conan-zlib-scripts"
+    license = "Zlib"
+
+    # download zlib sources
+    def source(self):
+        url = "https://zlib.net/zlib%s.zip" % self.version.replace(".","")
+        tools.get(url)
+
+    # compile using cmake
+    def build(self):
+        cmake = CMake(self)
+        zlib_folder = "%s/zlib-%s" % (self.source_folder, self.version)
+        cmake.verbose = True
+        cmake.configure(source_folder=zlib_folder)
+        cmake.build()
+        cmake.install()
+
+        if self.settings.os == "Windows":
+            libname = "zlib"
+            static_libname = "zlibstatic"
+            if self.settings.build_type == "Debug":
+                libname = "zlibd"
+                static_libname = "zlibstaticd"
+
+            # delete shared artifacts for static builds and the static library for shared builds
+            if self.options.shared == False:
+                os.remove('%s/bin/%s.dll' % (self.package_folder, libname))
+                os.remove('%s/lib/%s.lib' % (self.package_folder, libname))
+                os.rename('%s/lib/%s.lib' % (self.package_folder, static_libname), '%s/lib/%s.lib' % (self.package_folder, libname))
+            else:
+                os.remove('%s/lib/%s.lib' % (self.package_folder, static_libname))        
+
+    def package(self):
+        self.copy("*", dst="include", src='include')
+        self.copy("*.lib", dst="lib", src='lib', keep_path=False)
+        self.copy("*.dll", dst="bin", src='bin', keep_path=False)
+        self.copy("*.so", dst="lib", src='lib', keep_path=False)
+        self.copy("*.dylib", dst="lib", src='lib', keep_path=False)
+        self.copy("*.a", dst="lib", src='lib', keep_path=False)
+        
+    def package_info(self):
+        self.cpp_info.libs = tools.collect_libs(self)
+        self.cpp_info.includedirs = ['include']
+
+    def config_options(self):
+        # remove android specific option for all other platforms
+        if self.settings.os != "Android":
+            del self.options.android_ndk
